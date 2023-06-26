a57b4bce511bb5c11899a876f839f66f565115c2
SwatGetmann <swatgetmann@gmail.com>
2023-06-26 22:44:11 +0400

    nbg_gov: rename, move saving file functionality from basic request

--- nbg_gov.py
+++ nbg_gov.py
@@ -5,13 +5,18 @@ import json
 
 from urllib.parse import urlparse, parse_qs, urlencode
 
-def basic_request(url, headers, save_fpath):
+def basic_request(url, headers):
     result = requests.get(url, headers=headers)  
     print(result.status_code)
     print(result.json())
     print(result.cookies)
-    with open(save_fpath, "w") as file:
-        file.write(json.dumps(result.json()))
+    return result 
+
+
+def save_content(path, content):
+    with open(path, "w") as file:
+        file.write(content)
+
 
 def construct_url(template_url, start, end, currencies):
     test_url_parsed = urlparse(template_url)
@@ -35,6 +40,7 @@ def construct_url(template_url, start, end, currencies):
     print(test_url_parsed._replace(query=test_qs).geturl())
 
     return test_url_parsed._replace(query=test_qs).geturl()
+
 
 parser = argparse.ArgumentParser(description='Georgian currency API reader.')
 parser.add_argument('--start_dt', 
@@ -81,16 +87,22 @@ if __name__ == '__main__':
         "Cache-Control": "no-cache"
     }
     
-    basic_request(
+    result = basic_request(
         url=test_url_v2,
         headers=headers,
-        save_fpath='./results/test_01.json'
+    )
+    save_content(
+        path='./results/test_01.json', 
+        content=json.dumps(result.json())
     )
     
     test_url = "https://nbg.gov.ge/gw/api/ct/monetarypolicy/currencies/?currencies=USD&currencies=USD&end=2023-06-13T04%3A37%3A39.818Z&start=2023-05-24T04%3A37%3A39.818Z"
     
-    basic_request(
+    result = basic_request(
         url=test_url,
         headers=headers,
-        save_fpath='./results/test_02.json'
+    )
+    save_content(
+        path='./results/test_02.json',
+        content=json.dumps(result.json())
     )
